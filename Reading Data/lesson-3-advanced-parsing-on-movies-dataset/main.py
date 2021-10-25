c8b3d86e0206138403acf7d9ab9c347aed3c7c74
JinKim1221 <sdo7329@gmail.com>
2021-10-25 10:17:07 +0100

    practice read csv file in python and change null value to NaN make sure of data type

--- Reading Data/lesson-3-advanced-parsing-on-movies-dataset/main.py
+++ Reading Data/lesson-3-advanced-parsing-on-movies-dataset/main.py
@@ -1,7 +1,22 @@
 import numpy as np
 import pandas as pd
-
+# Requirements
+#- Use the appropriate separator.
+#- The given data doesn't have a defined header. Use the column names given in the `column_names` variable.
+#- Missing values are encoded as `?` characters. Parse those values as `NaN` objects.
+#- The `budget` column has commas separating the thousands, make sure the column is of float data type.
+#- The index of the DataFrame should be represented by the `movie_title` column.
 column_names = ['color', 'director_name', 'num_critic_for_reviews', 'duration',
                 'gross', 'movie_title', 'num_user_for_reviews',	'country',
                 'cotent_rating', 'budget', 'title_year', 'imdb_score', 'genre']
 
+movies = pd.read_csv('../files/movies.csv',
+                     sep='|', #each column is divided by '|'
+                     header=None, # no header was defined
+                     names=column_names, #given columns' name
+                     thousands=',', # make sure column 'budget' is of float data type.
+                     na_values='?', # value'?' is replaced to 'NaN'
+                     index_col='movie_title')
+
+
+print(movies['budget'].dtype)
--- Reading Data/lesson-3-advanced-parsing-on-movies-dataset/tests/test_budget.py
+++ Reading Data/lesson-3-advanced-parsing-on-movies-dataset/tests/test_budget.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
+movies = pd.read_csv('../files/movies.csv',
+                     sep='|', #each column is divided by '|'
+                     header=None, # no header was defined
+                     skiprows=3) # skip 3 rows
+
 def test_budget():
     assert movies.loc[:, 'budget'].min() == 105000000.0
+
+test_budget()

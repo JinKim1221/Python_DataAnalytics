ae451b93237439797c49374dbc584f6180085ff2
JinKim1221 <sdo7329@gmail.com>
2021-10-28 17:08:35 +0100

    practice read excel file and using sheet_name parameter

--- Reading Data/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/main.py
+++ Reading Data/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/main.py
@@ -1,4 +1,22 @@
 import numpy as np
 import pandas as pd
+# Requirements
+# Using the `playstore.xlsx` Excel file from the given `data_url` and:
+# - Save in a `playstore_df` variable the `Google_playstore` sheet. Use the first column as index.
+# - Save in a `content_id_df` variable the `Content_ID` sheet. Use `Content_ID` as index.
 
 data_url = 'https://github.com/ine-rmotr-projects/project-files/files/4086772/playstore.xlsx'
+playstore_df = pd.read_excel( data_url,
+                              sheet_name='Google_playstore',
+                              index_col=[0])
+
+content_id_df = pd.read_excel( data_url,
+                               sheet_name='Content_ID',
+                               index_col='Content_ID')
+
+content_id_head = pd.DataFrame({'Content_Rating': ['Everyone', 'Everyone', 'Everyone', 'Teen', 'Everyone']},
+                                index=[101, 101, 101, 102, 101])
+                            
+assert content_id_df.head().equals(content_id_head)
+print(content_id_df.head().equals(content_id_head))
+
--- Reading Data/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/solutions/solution_read_xlsx.py
+++ Reading Data/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/solutions/solution_read_xlsx.py
@@ -9,3 +9,5 @@ playstore_df = file.parse(index_col=0)
 
 content_id_df = file.parse('Content_ID', 
                           index_col='Content_ID')
+
+    

28b4c70789030973f432527d63df4095518b0be9
JinKim1221 <sdo7329@gmail.com>
2021-10-29 17:52:22 +0100

    practice read excel file and using sheet_name parameter

--- Reading Data/lesson-9-parse-artists-json-file/main.py
+++ Reading Data/lesson-9-parse-artists-json-file/main.py
@@ -1,3 +1,18 @@
 import numpy as np
 import pandas as pd
 
+# This exercise will take a few steps to complete successfully. Have a look at the `artists.json` file before starting so you have an idea of the structure and information contained.
+
+# - Read the `artists.json` into an `artists` DataFrame variable, without using `json_normalize`.
+# - Remove the `bio` column.
+# - Set the `name` column as index.
+# - Save it as `artists.csv` keeping the index.
+
+artists = pd.read_json('files/artists.json')
+
+artists = artists.drop(columns="bio").set_index('name')
+print(artists.head())
+
+artists.to_csv('artists.csv')
+# for col in artists.columns:
+#     print(col)

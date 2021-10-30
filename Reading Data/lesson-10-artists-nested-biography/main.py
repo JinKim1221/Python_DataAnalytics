e29ffa4f2d83f904c19f2eead1335abc25caadae
JinKim1221 <sdo7329@gmail.com>
2021-10-30 09:24:26 +0100

    practice read json file and using json_normalize to turn a json file into a data frame

--- Reading Data/lesson-10-artists-nested-biography/main.py
+++ Reading Data/lesson-10-artists-nested-biography/main.py
@@ -1,5 +1,23 @@
+from os import sep
+from sys import path
 import numpy as np
 import pandas as pd
 import json
 from pandas.io.json import json_normalize
 
+# This exercise will take a few steps to complete successfully. Have a look at the `artists.json` file before starting so you have an idea of the structure and information contained.
+
+# - Read the `artists.json` into an `artists` DataFrame variable, using `json_normalize`. Keep the default index.
+# - Using the `bio` column, create a new `biography` DataFrame variable with the bio of each artist.
+# - When creatng the `biography` DataFrame also add the `name` column.
+
+
+
+with open('files/artists.json') as file:
+    json_dict = json.load(file)
+
+artists = pd.json_normalize(json_dict)
+biography = json_normalize(json_dict, sep='_', record_path='bio', meta=['name'])
+
+
+print(biography)
--- Reading Data/lesson-10-artists-nested-biography/solutions/solution_read_json.py
+++ Reading Data/lesson-10-artists-nested-biography/solutions/solution_read_json.py
@@ -3,11 +3,13 @@ import pandas as pd
 import json
 from pandas.io.json import json_normalize
 
-with open('artists.json') as file:
+with open('files/artists.json') as file:
     json_dict = json.load(file)
 
-artists = json_normalize(json_dict)
 
-biography = json_normalize(json_dict,
+artists = pd.json_normalize(json_dict)
+
+biography = pd.json_normalize(json_dict,
                            record_path='bio',
                            meta=['name'])
+print(biography)

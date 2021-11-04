bcfd201426f8f810053a6b7e84857d9c4729eb48
JinKim1221 <sdo7329@gmail.com>
2021-11-04 17:14:32 +0000

    practice fetching data using rest api

--- Reading Data/lesson-16-discover-starwars-people/main.py
+++ Reading Data/lesson-16-discover-starwars-people/main.py
@@ -1,4 +1,20 @@
+from os import startfile
 import numpy as np
 import pandas as pd
 import requests
 
+# Requirements 
+# - Using the _requests_ library make a GET request to https://swapi.co/api/people/?format=json to get the `people` of StarWars universe.
+# - Store the JSON response into a `starwars_people_df` DataFrame variable.
+# - Filter blue-eyed characters on `blue_eyed_people_df`.
+# > SWAPI is a StarWars REST API with information about  `people`, `films`, `starships` and `planets` within the StarWars universe.
+
+
+req = requests.get('https://swapi.dev/api/people')
+json_dict = req.json()
+# print(req.json())
+starwars_people_df = pd.DataFrame.from_dict(json_dict['results'])
+# print(starwars_people_df)
+
+blue_eyed_people_df = starwars_people_df.loc[starwars_people_df['eye_color'] == 'blue']
+print(blue_eyed_people_df)
